cd7613d6b43113ceb90f5dffff1818122ff0cddd
Ced <49285085+its-Ced-It-Works@users.noreply.github.com>
2020-03-02 15:18:56 +0200

    fun animations!

--- script/indexJS.js
+++ script/indexJS.js
@@ -1,13 +1,20 @@
+function closeStart()
+{
+    document.getElementById("disclaimer").classList.add("fade-and-spin");
+    document.getElementById("close").classList.add("fade-and-spin");
+    document.getElementById("opening").innerHTML = "";
+    setTimeout(closeDisclaimer,2000);
+    document.body.style.overflow = 'auto';
+    document.html.style.overflow = 'auto';
+    
+};
+
 function closeDisclaimer()
 {
     document.getElementById("disclaimer").style.opacity = "0";
-    
     document.getElementById("close").style.width = "0";
     document.getElementById("close").style.opacity = "0";
     document.getElementById("opening").style.width = "0";
     document.getElementById("opening").style.opacity = "0";
     document.getElementById("disclaimer").style.width = "0";
-    document.body.style.overflow = 'auto';
-    document.html.style.overflow = 'auto';
-    
 };
