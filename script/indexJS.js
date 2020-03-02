a066ade0a404e404558ab852a8279587e62d1bca
Ced <49285085+its-Ced-It-Works@users.noreply.github.com>
2020-03-02 16:31:08 +0200

    animation improvements

--- script/indexJS.js
+++ script/indexJS.js
@@ -3,7 +3,7 @@ function closeStart()
     document.getElementById("disclaimer").classList.add("fade-and-spin");
     document.getElementById("close").classList.add("fade-and-spin");
     document.getElementById("opening").innerHTML = "";
-    setTimeout(closeDisclaimer,2000);
+    setTimeout(closeDisclaimer,1900);
     document.body.style.overflow = 'auto';
     document.html.style.overflow = 'auto';
     
@@ -11,10 +11,9 @@ function closeStart()
 
 function closeDisclaimer()
 {
-    document.getElementById("disclaimer").style.opacity = "0";
-    document.getElementById("close").style.width = "0";
-    document.getElementById("close").style.opacity = "0";
-    document.getElementById("opening").style.width = "0";
-    document.getElementById("opening").style.opacity = "0";
+    document.getElementById("disclaimer").remove();
     document.getElementById("disclaimer").style.width = "0";
+
+    document.getElementById("close").style.width = "0";
+    document.getElementById("opening").style.width = "0";
 };
